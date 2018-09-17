2ba5cf565ac866a3e914bd948423e101ca890f81
Khalid Mohamud <kmohamud1@outlook.com>
2018-09-17 13:58:27 -0700

    added server and problem controller

--- src/main/java/server/Application.java
+++ src/main/java/server/Application.java
@@ -1,11 +1,20 @@
 package server;
 
+import dbs.ThinkerDB;
+import models.Problem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
+        ThinkerDB db = new ThinkerDB();
+//        List<Problem> problems = db.getAllProblems();
+//        for (Problem  problem : problems) {
+//            System.out.println(problem.toString());
+//        }
     }
 }
--- src/main/java/server/ProblemController.java
+++ src/main/java/server/ProblemController.java
@@ -1,7 +1,24 @@
 package server;
 
-/**
- * Created by khalidmohamud on 9/16/18.
- */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api")
 public class ProblemController {
+
+    @RequestMapping("/problems")
+    @ResponseBody
+    public String init() {
+        return "This works";
+    }
+
+    @RequestMapping("/problems/all")
+    @ResponseBody
+    public String allProblems() {
+        return "yah";
+    }
 }
